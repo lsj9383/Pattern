b6b760e9eba46fca5d7000b126ff9b3a7953acaa
lsj <lsj9383@163.com>
2016-10-01 16:42:53 +0800

    update

---
+++ StatePattern/InnerDrive/InnerDrive/Machine/StateImpl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnerDrive.Machine
+{
+    class StateOne : State
+    {
+        StateMachine Machine = null;
+
+        public StateOne(StateMachine machine)
+        {
+            Machine = machine;
+        }
+
+        public void Next()
+        {
+            ;
+        }
+    }
+
+    class StateTwo : State
+    {
+        StateMachine Machine = null;
+
+        public StateTwo(StateMachine machine)
+        {
+            Machine = machine;
+        }
+
+        public void Next()
+        {
+            ;
+        }
+    }
+}
---
+++ StatePattern/InnerDrive/InnerDrive/Machine/StateMachine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace InnerDrive.Machine
+{
+    class StateMachine
+    {
+        public State stateOne = null;
+        public State stateTwo = null;
+
+        private State state = null;
+
+        public StateMachine()
+        {
+            stateOne = new StateOne(this);
+            stateTwo = new StateOne(this);
+            state = stateOne;
+        }
+
+        public void Start()
+        {
+            while (true)
+            {
+                state.Next();           //状态操作与更迭
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
