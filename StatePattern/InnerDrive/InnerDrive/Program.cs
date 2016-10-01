849375bd1a27fa182c7fa8450ac1ea53912877c4
lsj <lsj9383@163.com>
2016-10-01 17:33:33 +0800

    update

--- StatePattern/InnerDrive/InnerDrive/Program.cs
+++ StatePattern/InnerDrive/InnerDrive/Program.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using InnerDrive.Machine;
+
 namespace InnerDrive
 {
     class Program
     {
         static void Main(string[] args)
         {
+            new StateMachine().Start();
         }
     }
 }
